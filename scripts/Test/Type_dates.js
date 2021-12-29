d716f68360930e082f4507efedb55e9e775bc92c
renanll <renan_ll@hotmail.com>
2021-12-29 18:56:36 -0300

    TEST:Aprendendo sobre tipos de dados

---
+++ scripts/Test/Type_dates.js
@@ -0,0 +1,87 @@
+//Aqui temos um printo //
+console.log('Hola mundo ')
+
+//String 
+'', "", ``
+console.log('Renan')
+//`` permite que da pra usar multiplas linhas e também expressões de linguagens.
+console.log(1 + 1)
+console.log(`renan ${1 + 1}`)
+
+//Tipo de dado Number
+/*inteiros 
+    reais floats
+    NaN  not number
+    Infinity infinito
+*/
+console.log(1 / 'dale')
+
+console.log(Infinity)
+
+/*
+    Boolean
+    true  verdadeiro 
+    false falso
+*/
+console.log(true)
+
+/*
+    undefined
+        não defenido
+    null 
+        nulo 
+        objeto que não tem nada dentro
+        diferente de indefinido
+*/
+
+console.log(null === undefined)
+
+/*
+    Object
+    Objeto
+    Propriedades / atributos
+    Funcionalidades / Método 
+    { propriedades: "valor"}
+*/
+
+console.log({
+    name: 'Renan',
+    idade: '25',
+    andar: function () {
+        console.log('andar');
+    }
+})
+
+/*
+    Array Vetores
+    Uma lista 
+    Agrupamento de dados
+    ['renan', 30]
+
+*/
+
+console.log(['Renan', 'Ovos', 23, '4'])
+
+//Tipos de dados
+
+// Data types
+//Primitive / Primitive value
+//Stryctural
+//Structural primitive
+
+// Primitivos
+// String
+// Number
+// Boolean
+// undefined
+// Symbol
+// BigInt
+
+// Estruturais
+// Object
+// Array
+// Map
+// Set
+// Data
+// ...
+// Function
